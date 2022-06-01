9e2de7bc2f2c570fdb17fb7f34f19174b3e5e9ad
Erwin Méndez <erwinmendez0911@hotmail.com>
2022-05-31 21:21:04 -0400

    Fixing margin top

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Toast: React.FC<{ message: string }> = ({ message }) => {
   return (
-    <div className="fixed mt-[30%] bg-white text-gray-900 px-8 py-4 font-bold rounded-md text-lg border border-gray-300">
+    <div className="absolute mt-[150px] bg-white text-gray-900 px-8 py-4 font-bold rounded-md text-lg border border-gray-300 shadow-xl">
       {message}
     </div>
   );
