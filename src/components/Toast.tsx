4522e79f8514027ce50aa8eb8383889e2dd45467
Erwin Méndez <erwinmendez0911@hotmail.com>
2022-06-06 22:29:27 -0400

    Adding z-index

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Toast: React.FC<{ message: string }> = ({ message }) => {
   return (
-    <div className="absolute mt-[150px] bg-white text-gray-900 px-8 py-4 font-bold rounded-md text-lg border border-gray-300 shadow-xl">
+    <div className="absolute mt-[150px] bg-white text-gray-900 px-8 py-4 font-bold rounded-md text-lg border border-gray-300 shadow-xl z-10">
       {message}
     </div>
   );
